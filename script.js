4795f60da0c07cf415c3184db374409983efeab8
yuiartemisa <yuiiama31@gmail.com>
2023-06-09 11:52:33 -0300

    adjust avatar light

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function toggleMode() {
   const html = document.documentElement;
+  const body = document.body;
   html.classList.toggle("light");
+  body.classList.toggle("light");
 
   //pegar a tag img
   const img = document.querySelector("#profile img");
 
   //substituir a imagem
   if (body.classList.contains("light")) {
-    //se tiver light mode , adicionar a imagem light
+    //se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png");
   } else {
     //se tiver sem light mode, manter a imagem normal
