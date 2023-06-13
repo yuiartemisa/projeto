759518ee5d917fa3ba9ad6c9e1c667ee42442f00
yuiartemisa <yuiiama31@gmail.com>
2023-06-13 13:42:29 -0300

    changes profile avatar

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ function toggleMode() {
     img.setAttribute("src", "./assets/avatar-light.png");
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("src", "./assets/avatar-dark.png");
   }
 }
